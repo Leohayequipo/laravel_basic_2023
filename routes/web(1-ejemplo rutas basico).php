1b0204efc569ba091315bd8c86d459a6079fc41e
Leohayequipo <54806458+Leohayequipo@users.noreply.github.com>
2023-08-19 20:22:44 -0300

    Retornar una vista

---
+++ routes/web(1-ejemplo rutas basico).php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('blog',function(){
+    return "entre a post";
+});
+
+Route::get('blog/{slug}',function($slug){
+    return $slug;
+});
+
+Route::get('buscar',function(Request $request){
+    return $request->all();
+    //objeto metodo all
+});
