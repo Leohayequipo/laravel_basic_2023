699873a8e55e59392aff273fcabe3718adee8fdb
Leohayequipo <54806458+Leohayequipo@users.noreply.github.com>
2023-08-21 12:20:48 -0300

    Template y usando name en las rutas

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Listado</h1>
+@extends('template')
+
+@section('content')
+<h1>Listado</h1>
     @foreach($posts as $post)
     <p>
         <strong>
             {{ $post['id'] }}
-            <a href="">
+            <a href="{{  route('post',$post['slug'])}}">
                 {{$post['title']}}
             </a>
         </strong>
     </p>
     @endforeach
-</body>
-</html>
+@endsection
