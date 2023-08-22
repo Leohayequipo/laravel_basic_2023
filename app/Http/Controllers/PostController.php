4112e8f65075e69e4453546a3c9c8201f00ad727
Leohayequipo <54806458+Leohayequipo@users.noreply.github.com>
2023-08-22 20:40:17 -0300

    Crear formulario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,10 +8,28 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    //metodo    
     public function index(){
         return view('posts.index',
         [
             'posts'=>Post::latest()->paginate()
+        ]);
+    }
+    //las rutas apuntan a los metodos
+    public function destroy(Post $post){
+        $post->delete();
+        return back();
+    }
+    //metodo    
+    public function create(){
+        return view('posts.create');
+       
+    }
+     //metodo    
+    public function edit(Post $post){
+        return view('posts.edit',
+        [
+            'post'=>$post
         ]);
     }
 }
