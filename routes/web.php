c12acc72429e637f74d2f74c1a6d9e2d9d8c1429
Leohayequipo <54806458+Leohayequipo@users.noreply.github.com>
2023-08-23 17:29:48 -0300

    Fin!

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,8 @@ Route::controller(PageController::class)->group(function (){
 
 });
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('dashboard', 'posts')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -30,6 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('posts',PostController::Class)->except(['show']);
+Route::resource('posts',PostController::Class)->middleware(['auth', 'verified'])->except(['show']);
 
 require __DIR__.'/auth.php';
